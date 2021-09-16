0a5851f1920e1f2c3d97c3a4495944fe3d0c568f
kotulaigen <kotulai.gen@gmail.com>
2021-09-16 17:07:10 +0300

    added registration and authentification

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,7 @@ router.post(
     "/signup",
     [
         check("username", "please enter a valid username")
-        .notEmpty(),
+            .notEmpty(),
         check("email", "please enter valid email").isEmail(),
         check("password", "please enter valid password").isLength({
             min: 6
@@ -19,7 +19,7 @@ router.post(
     ],
     async (req, res) => {
         const errors = validationResult(req);
-        if (!errors.isEmpty()){
+        if (!errors.isEmpty()) {
             return res.status(400).json({
                 errors: errors.array()
             });
@@ -30,13 +30,13 @@ router.post(
             email,
             password
         } = req.body;
-        
-        try{
+
+        try {
             let user = await User.findOne({
                 email
             });
-            
-            if(user){
+
+            if (user) {
                 return res.status(400).json({
                     msg: 'User Already Exists'
                 });
@@ -62,18 +62,74 @@ router.post(
             jwt.sign(
                 payload,
                 'randomString', {
-                    expiresIn: 10000
-                },
-                (err,token) => {
-                    if(err) console.log(err);
+                expiresIn: 10000
+            },
+                (err, token) => {
+                    if (err) console.log(err);
                     res.status(200).json({
                         token
                     });
                 }
             );
-        } catch(err){
+        } catch (err) {
             console.log(err.message);
             res.status(500).send('Error in Saving');
+        }
+    }
+);
+
+router.post("/login",
+    [
+        check("email", "Please enter a valid email").isEmail(),
+        check("password", "Please enter a valid password").isLength({
+            min: 6
+        })
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).jsom({
+                errors: errors.array()
+            });
+        }
+
+        const { email, password } = req.body;
+        try {
+            let user = await User.findOne({
+                email
+            });
+            if (!user) return res.status(400).json({
+                message: "User not exist"
+            });
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) return res.status(400).json({
+                message: "Incorrect password"
+            });
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(payload,
+                "randomString", {
+                expiresIn: 3600
+            },
+                (err, token) => {
+                    if (err) throw err;
+                    res.status(200).json({
+                        token
+                    });
+                }
+            );
+        } catch (e) {
+            console.error(e);
+            res.status(500).json({
+                message: "Server error"
+            });
         }
     }
 );
