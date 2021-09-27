887d285a3caac137a90d54b818ddc7fc479bd1f3
kotulaigen <kotulai.gen@gmail.com>
2021-09-27 17:09:10 +0300

    added constants and private chats

--- app.js
+++ app.js
@@ -3,18 +3,16 @@ const InitiateMongoServer = require('./config/db')
 const cors = require('cors')
 const user = require('./routes/user')
 const Message = require('./models/message')
+const { client, PORT, WSPORT } = require('./constants/constants')
 
 InitiateMongoServer()
 
-const users = {
-    
-}
+const users = {}
 
 const app = express()
-const PORT = process.env.PORT || 4000;
-const io = require("socket.io")(8080, {
+const io = require("socket.io")(WSPORT, {
     cors: {
-        origin: ['http://13e3-91-218-98-249.ngrok.io']
+        origin: [client]
     }
 });
 
@@ -24,20 +22,29 @@ app.use('/user', user)
 app.listen(PORT, (req, res) => {
     console.log(`Server started at PORT ${PORT}`)
 })
+
 io.on('connection', (socket) => {
-    socket.on('user-log-in', (name, socket) => {
-        users[socket] = name
-        socket.emit('user-connected', name)
+    socket.on('user-log-in', (user, userSocket) => {
+        users[user] = userSocket
+        io.emit('users-connected', users)
+        console.log(users)
     })
-    socket.on('message', (msg, user) => {
+    socket.on('message', (msg, user, room) => {
         const message = new Message({
             user: user,
             message: msg,
         })
-        message.save()    
-        io.emit('get-message', message.message, message.user, message.socketId)
+        if (room === '') {
+            socket.broadcast.emit('get-message', message.message, message.user, message.socketId)
+            message.save()
+        } else {
+            socket.to(room).emit('get-message', message.message, message.user, message.socketId)
+        }
     });
-
+    socket.on('disconnect', () => {
+        delete users[socket.id]
+        io.emit('user-disconnected', users)
+    })
 });
 
 
---
+++ constants/constants.js
@@ -0,0 +1,5 @@
+const client = 'http://6f53-91-218-98-249.ngrok.io'
+const PORT = '4000'
+const WSPORT = '8080'
+
+module.exports = {client, PORT, WSPORT}
