804496dd53152fc774cc99a33de0ba64864edad1
kotulaigen <kotulai.gen@gmail.com>
2021-09-28 15:40:20 +0300

    delete user func, add room func, join room

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ const { client, PORT, WSPORT } = require('./constants/constants')
 InitiateMongoServer()
 
 const users = {}
+const rooms = []
 
 const app = express()
 const io = require("socket.io")(WSPORT, {
     cors: {
-        origin: [client]
+        origin: client
     }
 });
 
@@ -23,26 +24,44 @@ app.listen(PORT, (req, res) => {
     console.log(`Server started at PORT ${PORT}`)
 })
 
+function deleteUser(socket) {
+    for (let key in users) {
+        if (users[key] === socket) delete users[key]
+    }
+}
+
 io.on('connection', (socket) => {
+    io.emit('room-added', rooms)
+    socket.on('add-room', (room) => {
+        rooms.push(room)
+        io.emit('room-added', rooms)
+        console.log(rooms)
+    })
+    socket.on('join-room', room => {
+        socket.join(room)
+        console.log(socket.id, 'joined the room', room)
+    })
     socket.on('user-log-in', (user, userSocket) => {
         users[user] = userSocket
-        io.emit('users-connected', users)
+        io.emit('users-connected', users, rooms)
         console.log(users)
     })
-    socket.on('message', (msg, user, room) => {
+    socket.on('message', (msg, user, roomName, roomId) => {
         const message = new Message({
             user: user,
             message: msg,
+            roomName: roomName,
+            roomId: roomId
         })
-        if (room === '') {
+        if (roomId === '') {
             socket.broadcast.emit('get-message', message.message, message.user, message.socketId)
             message.save()
         } else {
-            socket.to(room).emit('get-message', message.message, message.user, message.socketId)
+            socket.to(roomId).emit('get-message', message.message, message.user, message.socketId)
         }
     });
     socket.on('disconnect', () => {
-        delete users[socket.id]
+        deleteUser(socket.id)
         io.emit('user-disconnected', users)
     })
 });
--- constants/constants.js
+++ constants/constants.js
@@ -1,5 +1,5 @@
-const client = 'http://6f53-91-218-98-249.ngrok.io'
+const client = "http://0bea-91-218-98-249.ngrok.io"
 const PORT = '4000'
 const WSPORT = '8080'
 
-module.exports = {client, PORT, WSPORT}
+module.exports = { client, PORT, WSPORT }
